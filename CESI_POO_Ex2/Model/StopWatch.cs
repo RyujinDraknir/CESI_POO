bca5fa89f47e0287f3f3fd3d9cb23765acfeb28d
Arthur Laroque <laroque.e1903405@etud.univ-ubs.fr>
2022-02-07 14:33:31 +0100

    j'ai nettoyé lol

---
+++ CESI_POO_Ex2/Model/StopWatch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CESI_POO_Ex2.Model
+{
+    public class StopWatch
+    {
+        private DateTime? StartTime { get; set; }
+        private DateTime? EndTime { get; set; }
+        private bool isRunning { get; set; }
+        public TimeSpan? TimeSpan { get; set; }
+
+        public StopWatch() 
+        {
+            isRunning = false;
+        }
+
+        public void Start()
+        {
+            if (isRunning)
+                throw new InvalidOperationException("StopWatch already started.");
+            StartTime = DateTime.UtcNow;
+            isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!isRunning || !StartTime.HasValue )
+                throw new InvalidOperationException("StopWatch not started.");
+            EndTime = DateTime.UtcNow;
+            TimeSpan = new TimeSpan(EndTime.Value.Ticks - StartTime.Value.Ticks);
+            isRunning = false;
+        }
+
+        public void Reset()
+        {
+            StartTime = null;
+            EndTime = null;
+            TimeSpan = null;
+            isRunning = false;
+        }
+
+        public void PrintResult()
+        {
+            if(TimeSpan == null)
+                throw new InvalidOperationException("StopWatch has no results recorded");
+            Console.WriteLine(this);
+        }
+
+        public override string ToString()
+        {
+            return "Start Time : " + StartTime.Value.ToString("HH:mm:ss:ffffff dd/MM/yyyy")
+                + "\n" + "Stop Time : " + EndTime.Value.ToString("HH:mm:ss:ffffff dd/MM/yyyy")
+                + "\n" + "Result : " + TimeSpan.Value.ToString("G");
+        }
+    }
+}
---
+++ CESI_POO_Ex2/Program.cs
@@ -0,0 +1,71 @@
+﻿using CESI_POO_Ex2.Model;
+using System;
+using System.Threading;
+
+namespace CESI_POO_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+
+            try
+            {
+                stopWatch.Stop();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            try
+            {
+                stopWatch.Start();
+                stopWatch.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Thread.Sleep(5000);
+                stopWatch.Stop();
+
+                stopWatch.PrintResult();
+            }
+
+
+            stopWatch.Start();
+            Thread.Sleep(2000);
+            stopWatch.Stop();
+
+
+            stopWatch.Reset();
+            stopWatch.Start();
+            Thread.Sleep(10000);
+            stopWatch.Stop();
+
+            stopWatch.PrintResult();
+
+            for(int i = 0; i < 2; i++)
+            {
+                stopWatch.Reset();
+
+                Console.WriteLine("Press enter to start StopWatch");
+                Console.ReadLine();
+                stopWatch.Start();
+
+                Console.WriteLine("Press enter to stop StopWatch");
+                Console.ReadLine();
+                stopWatch.Stop();
+
+                Console.Clear();
+                stopWatch.PrintResult();
+
+            }
+        }
+    }
+}
